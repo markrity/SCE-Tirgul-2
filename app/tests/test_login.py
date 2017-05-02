d727ffae62fb44d397cecbbcda8a611affa6cf9f
Eddy Medvednic <medvednic@gmail.com>
2017-05-02 14:18:48 +0300

    added invalid user test

--- app/tests/test_login.py
+++ app/tests/test_login.py
@@ -10,15 +10,22 @@ class LoginTestCase(unittest.TestCase):
         self.tester = self.app.test_client(self)
         self.tester.testing = True
 
-    def test_manager(self):
+    def test_manager_page(self):
         response = self.tester.get('/app/manager')
         self.assertEqual(response.status_code, 404)
 
-    def test_login(self):
-
-        response = self.tester.post('login', data=dict(first_name='invalid', last_name='invalid'))
+    def test_no_id_login(self):
+        response = self.tester.post('login', data=dict(first_name='myname', last_name='mylastname'))
         self.assertEqual(response.status_code, 404)
         err = 'טופס לא חוקי'
+        assert str.encode(err) in response.data
+
+    def test_invalid_user(self):
+        credentials = {'first_name': 'unexisting', 'last_name': 'lastname', 'id_num': 1234}
+        response = self.tester.post('login', data=credentials,
+                                    follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
+        err = 'משתמש לא קיים במערכת'
         assert str.encode(err) in response.data
 
 
