dd3dce4a8180d8528ec2fe95c1f3f4677194d417
Mark Davydov <markrity@gmail.com>
2017-06-16 14:54:55 -0700

    -Fixed the full valid user with vote test.

--- app/tests/tests_integration.py
+++ app/tests/tests_integration.py
@@ -5,6 +5,10 @@ import unittest
 
 from flask_testing import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 from app import app, db
 from app.models import User, Party
@@ -68,22 +72,23 @@ class AppTestCase(LiveServerTestCase):
 
     def test_valid_user_vote_selenium(self):
         self.valid_user = User(111111, 'firstName', 'lastName', False)
-        self.valid_party = Party(u'עלה ירוק', 'static/images/yarok.jpeg', 0)
+        self.valid_party = Party('yarok', 'static/images/yarok.jpeg', 0)
         self.first_name = self.driver.find_element_by_id('first_name')
         self.last_name = self.driver.find_element_by_id('last_name')
         self.id_num = self.driver.find_element_by_id('id_num')
         self.login_button = self.driver.find_element_by_id('login_button')
-        self.vote_button = self.driver.find_element_by_id('vote_button')
-        self.vote_button_yes = self.driver.find_element_by_id('vote_button_yes')
-        self.valid_party_thumb = self.driver.find_element_by_id(u'עלה ירוק')
         self.first_name.send_keys(self.valid_user.first_name)
         self.last_name.send_keys(self.valid_user.last_name)
         self.id_num.send_keys(self.valid_user.id)
         self.login_button.submit()
+        self.valid_party_thumb = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/form/div[1]/div/label/div/div/img')))
         self.valid_party_thumb.click()
-        self.vote_button.click()
+        self.vote_button = self.driver.find_element_by_id('button_vote')
+        self.vote_button.send_keys(Keys.ENTER)
+        self.vote_button_yes = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.ID, 'button_vote_yes')))
         self.vote_button_yes.submit()
         assert 'Flask Intro' in self.driver.title
+
 
 if (__name__ == '__main__'):
     unittest.main()
